a3112e4399894e87e40a4c5c8598caaa59998666
Austin Liu <austinhliu@gmail.com>
2023-06-11 23:40:37 -0700

    fix: increase splash screen time

--- multiplayer/game/scenes/splash.py
+++ multiplayer/game/scenes/splash.py
@@ -12,8 +12,9 @@ class SplashScene(Scene):
         self.background.fill((0, 0, 0))
         self.alpha_start = 0
         self.alpha_end = 255
-        self.fade_in_duration = 2000  # in milliseconds
-        self.fade_out_duration = 2000  # in milliseconds
+        self.fade_in_duration = 3000  # in milliseconds
+        self.fade_out_duration = 3000  # in milliseconds
+        self.hold_duration = 250
         self.sound = pygame.mixer.Sound("assets/sounds/logo_sound.mp3")
         # self.next = "main_menu"
 
@@ -25,7 +26,7 @@ class SplashScene(Scene):
     def draw(self, screen):
         screen.blit(self.background, (0, 0))
         screen.blit(self.logo, (SCREEN_WIDTH//2 - self.logo.get_width()//2, SCREEN_HEIGHT//2 - self.logo.get_height()//2))
-        screen.blit(self.label, (SCREEN_WIDTH//2 - self.label.get_width()//2, SCREEN_HEIGHT//2 + self.logo.get_height()//2 + 40))
+        screen.blit(self.label, (SCREEN_WIDTH//2 - self.label.get_width()//2, SCREEN_HEIGHT//2 + self.logo.get_height()//2 + 65))
 
     def update(self):
         elapsed_time = pygame.time.get_ticks() - self.start_time
@@ -34,6 +35,8 @@ class SplashScene(Scene):
             alpha = self.alpha_start + int((self.alpha_end - self.alpha_start) * elapsed_time / self.fade_in_duration)
         elif elapsed_time < (self.fade_in_duration + self.fade_out_duration):
             alpha = self.alpha_end - int((self.alpha_end - self.alpha_start) * (elapsed_time - self.fade_in_duration) / self.fade_in_duration)
+        elif elapsed_time < (self.fade_in_duration + self.fade_out_duration + self.hold_duration):
+            ...
         else:
             self.done = True
 
